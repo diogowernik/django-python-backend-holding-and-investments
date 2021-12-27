1a18830f2e07c90094eb18e7127f4c5c6dbdb6f5
Diogo Wernik <11983260+diogowernik@users.noreply.github.com>
2021-12-26 23:13:05 -0300

    radar creation finished

--- portfolios/serializers.py
+++ portfolios/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from . import models
+from radars.serializers import RadarSerializer
 
 # Minha Holding
 
@@ -9,6 +10,8 @@ class PortfolioSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'image')
 
 class PortfolioDetailSerializer(serializers.ModelSerializer):    
+    radars = RadarSerializer(many=True, read_only=True)
+
     class Meta:
         model = models.Portfolio
-        fields = ('id', 'name', 'image')
+        fields = ('id', 'name', 'image', 'radars')
