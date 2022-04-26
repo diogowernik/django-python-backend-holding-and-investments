c7e7f607b10bc180a1e4303da089cb54b7304ec2
Diogo Wernik <11983260+diogowernik@users.noreply.github.com>
2022-04-26 18:08:15 -0300

    Automatic slug creation

--- categories/admin.py
+++ categories/admin.py
@@ -2,8 +2,25 @@ from django.contrib import admin
 from django.contrib.admin import RelatedFieldListFilter
 from . import models
 
-# Register your models here.
 
-admin.site.register(models.Category)
-admin.site.register(models.SetorFii)
-admin.site.register(models.SetorCrypto)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ['slug']
+
+
+class SetorFiiAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ['slug']
+
+
+class SetorCryptoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ['slug']
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.SetorFii, SetorFiiAdmin)
+admin.site.register(models.SetorCrypto, SetorCryptoAdmin)
